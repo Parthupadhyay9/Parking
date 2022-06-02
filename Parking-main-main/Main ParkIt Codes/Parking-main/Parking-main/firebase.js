263ffab8d683f87d69fb67c8900024fe2b35bd5d
Parthupadhyay9 <65353682+Parthupadhyay9@users.noreply.github.com>
2022-06-02 23:09:59 +0530

    Add files via upload

---
+++ Parking-main-main/Main ParkIt Codes/Parking-main/Parking-main/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAvpN8zpmjVzoM0aNbmfeEc0yPUYmvFuKs",
+    authDomain: "parking-b060d.firebaseapp.com",
+    projectId: "parking-b060d",
+    storageBucket: "parking-b060d.appspot.com",
+    messagingSenderId: "1022081370305",
+    appId: "1:1022081370305:web:5041e0c64d680a78ba2c40",
+    measurementId: "G-P5SHPLVKR2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.analytics();
